5d89e81cf08dd2b63c155d9c742fa10ff8859c3f
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2020-06-12 17:15:08 -0700

    jk oop2 done

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -1,7 +1,3 @@
-# To the GroundVehicle class, add method drive() that returns "vroooom".
-#
-# Also change it so the num_wheels defaults to 4 if not specified when the
-# object is constructed.
 
 class GroundVehicle():
     def __init__(self, num_wheels=4):
@@ -10,13 +6,6 @@ class GroundVehicle():
     def drive(self, noise="vroooom"):
         return noise
 
-
-# Subclass Motorcycle from GroundVehicle.
-#
-# Make it so when you instantiate a Motorcycle, it automatically sets the number
-# of wheels to 2 by passing that to the constructor of its superclass.
-#
-# Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 class Motorcycle(GroundVehicle):
     def __init__(self, num_wheels=2):
@@ -34,6 +23,9 @@ vehicles = [
     Motorcycle(),
 ]
 
-# Go through the vehicles list and print the result of calling drive() on each.
 
-# TODO
+print(vehicles[0].drive())
+print(vehicles[1].drive())
+print(vehicles[2].drive())
+print(vehicles[3].drive())
+print(vehicles[4].drive())
